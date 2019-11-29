b3b60f488f4f56852b4180aad6b8aba734739989
filli1303 <bpfilip@gmail.com>
2019-11-29 16:40:20 +0100

    cleaned console some more

--- private/src/js/index.js
+++ private/src/js/index.js
@@ -172,6 +172,4 @@ let deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
 	// Stash the event so it can be triggered later.
 	deferredPrompt = e;
-
-	console.log(deferredPrompt)
 });
